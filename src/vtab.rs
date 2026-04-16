f24e22f2d153ade0a5095f106e6a174e777534c9
Brian Macy <brianmacy@gmail.com>
2026-04-16 07:00:49 -0400

    Revert "Apply cargo fmt to fix CI formatting check"
    This reverts commit 4e4bb6fa5460f57cdc1da3b24618ea36088dd78c.

--- src/vtab.rs
+++ src/vtab.rs
@@ -617,9 +617,7 @@ impl HnswStmtCache {
 impl Drop for HnswStmtCache {
     fn drop(&mut self) {
         // Safety: finalize() checks for null pointers and handles them gracefully
-        unsafe {
-            self.finalize();
-        }
+        unsafe { self.finalize(); }
     }
 }
 
